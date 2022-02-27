79cee4cbc4d39aae620f7065f764b2b33858db20
jerpelo <jasmine.erpelo@gmail.com>
2022-02-27 15:01:28 -0500

    implemented ability to edit channels

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -5,9 +5,6 @@ import { UserList } from "./";
 import { CloseCreateChannel } from "../assets";
 
 const ChannelNameInput = ({ channelName = "", setChannelName }) => {
-  const { client, setActiveChannel } = useChatContext();
-  const [selectedUsers, setSelectedUsers] = useState([client.userID || ""]);
-
   const handleChange = (event) => {
     event.preventDefault();
 
